9e6ee20c429a3766d6f05d77fa41364c77b690c7
augustouce <aysalazar@uce.edu.ec>
2023-12-18 19:54:33 -0500

    taller17

--- src/main/java/com/uce/edu/Pa2U2P5AsApplication.java
+++ src/main/java/com/uce/edu/Pa2U2P5AsApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.uce.edu.repository.modelo.Alumno;
+import com.uce.edu.repository.modelo.Ciudadano;
+import com.uce.edu.repository.modelo.Empleado;
 import com.uce.edu.repository.modelo.Estudiante;
 import com.uce.edu.service.IAlumnoService;
+import com.uce.edu.service.ICiudadanoService;
+import com.uce.edu.service.IEmpleadoService;
 import com.uce.edu.service.IEstudianteService;
 
 @SpringBootApplication
 public class Pa2U2P5AsApplication implements CommandLineRunner {
+
 	@Autowired
-	private IEstudianteService estudianteService;
+	private IEmpleadoService empleadoService;
+	
 	@Autowired
-	private IAlumnoService alumnoService;
+	private ICiudadanoService ciudadanoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U2P5AsApplication.class, args);
@@ -27,25 +34,32 @@ public class Pa2U2P5AsApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		Estudiante estu = new Estudiante();
-		estu.setNombre("Augusto");
-		estu.setApellido("Salazar");
-		estu.setCedula("1752083905");
-		estu.setFechaNacimiento(LocalDateTime.of(1999, 07, 07, 07, 07));
+	
+		
+		
+		
+		
+		Ciudadano c= new Ciudadano();
+		c.setNombre("Rony");
+		c.setApellido("Salazar");
+	
+		
+		
+		Empleado e= new Empleado();
+		e.setFechaIngreso(LocalDateTime.now());
+		e.setSalario(new BigDecimal(100));
+		c.setEmpleado(e);
+	
+		
+		e.setCiudadano(this.ciudadanoService.buscar(3));
+		
+	
+		
 
-		// this.estudianteService.guardar(estu);
-
-		// System.out.println(this.estudianteService.buscar(16));
-
-		Alumno alu = new Alumno();
-		alu.setNombre("Augusto");
-		//this.alumnoService.guardar(alu);
-		//System.out.println(this.alumnoService.buscar(4));
-		this.alumnoService.eliminar(4);
-	/*	Alumno alu1 =this.alumnoService.buscar(1);
-		alu1.setNombre("Jazmin");
-		this.alumnoService.actualizar(alu1);
-*/
+	
+		
+		//System.out.println(this.ciudadanoService.buscar(3));
+		
 	}
 
 }
